db37dd7fd497d8f5d64b4584edea89ae1eb8a4bd
Javier Rojas <javierrojas14.jr@gmail.com>
2025-03-24 16:30:50 -0300

    Inclye funcion para leer los casos de macroprocesos

--- proyeccion_rdr/features.py
+++ proyeccion_rdr/features.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+import pandas as pd
 import typer
 from loguru import logger
 from tqdm import tqdm
@@ -7,6 +8,33 @@ from tqdm import tqdm
 from proyeccion_rdr.config import PROCESSED_DATA_DIR
 
 app = typer.Typer()
+
+
+def leer_casos_macroprocesos(ruta_archivo):
+    # Obtiene los casos por macroproceso por region
+    casos_macroproceso_por_region = pd.read_excel(
+        ruta_archivo, sheet_name="casos_macroproceso_por_region"
+    )
+
+    # Obtiene los casos por macroproceso consolidados
+    casos_macroproceso_consolidados = pd.read_excel(
+        ruta_archivo, sheet_name="casos_macroproceso_consolidado"
+    )
+
+    # Pone como indice el diagnostico
+    casos_macroproceso_por_region = casos_macroproceso_por_region.set_index("Diagnostico")
+    casos_macroproceso_consolidados = casos_macroproceso_consolidados.set_index("Diagnostico")
+
+    # Cambia la columna de los diagnosticos contenidos
+    casos_macroproceso_por_region["Diagnosticos Contenidos"] = casos_macroproceso_por_region[
+        "Diagnosticos Contenidos"
+    ].str.split(", ")
+
+    casos_macroproceso_consolidados["Diagnosticos Contenidos"] = casos_macroproceso_consolidados[
+        "Diagnosticos Contenidos"
+    ].str.split(", ")
+
+    return casos_macroproceso_por_region, casos_macroproceso_consolidados
 
 
 @app.command()
