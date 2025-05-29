c83162350f064bc00729e06f2334123c3d2fe4f3
Javier Rojas <javierrojas14.jr@gmail.com>
2025-05-29 14:51:58 -0400

    Lee correctamente las bases

--- proyeccion_rdr/produccion/a05_unidades_de_apoyo.py
+++ proyeccion_rdr/produccion/a05_unidades_de_apoyo.py
@@ -1,11 +1,13 @@
 import numpy as np
 import pandas as pd
+import polars as pl
 
 
 def leer_cae(ruta):
-    df = pd.read_csv(ruta, dtypes={"id_paciente": str})
+    df = pd.read_csv(ruta, dtype={"id_paciente": str})
     return df
 
 
 def leer_grd(ruta):
-    df = pd.read_csv(ruta, dtypes={"id_paciente": str})
+    df = pd.read_csv(ruta, dtype={"id_paciente": str})
+    return df
